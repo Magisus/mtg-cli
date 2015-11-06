5c1524b28af449694b88ccf57315bd61b4bb09b7
Maggie Dreyer <mdreyer@lclark.edu>
2015-11-05 23:59:34 -0800

    Saves data file in a permanent location; only downloads if file is not already present.

--- lib/mtg_cli/cli.rb
+++ lib/mtg_cli/cli.rb
@@ -7,8 +7,22 @@ module MtgCli
     end
 
     def run
-      file = Downloader.new('http://mtgjson.com/json/AllSets.json').get
-      binding.pry
+      data_directory = File.expand_path("../../../data", __FILE__)
+      data_file = data_directory + "/card-data.json"
+      if !File.exist?(data_directory)
+        Dir.mkdir(data_directory)
+        fetch_card_data(data_file)
+      elsif !File.exist?(data_file)
+        fetch_card_data(data_file)
+      else
+        puts "Card data present; no download required"
+      end
+    end
+
+    def fetch_card_data(destination)
+      tempfile = Downloader.new("http://mtgjson.com/json/AllSets.json").get
+      file = File.new(destination, 'w')
+      file.write(tempfile.read)
     end
   end
 end
