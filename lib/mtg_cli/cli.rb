1396283a386f33aa752a841fba4ab87b60e4cbbc
Connor Lay <connor.lay@me.com>
2015-11-16 22:23:25 -0800

    [refactor] use guard statements instead of if-else blocks

--- lib/mtg_cli/cli.rb
+++ lib/mtg_cli/cli.rb
@@ -1,30 +1,29 @@
 module MtgCli
   class Cli
+    attr_reader :arg
+
+    DATA_DIRECTORY = File.expand_path('../../../data', __FILE__)
+    DATA_SOURCE    = 'http://mtgjson.com/json/AllSets.json'
+
     def initialize(arg)
       @arg = arg
     end
 
     def run
       check_card_data
-    end
+   end
 
     private
 
     def check_card_data
-      data_directory = File.expand_path('../../../data', __FILE__)
-      data_file = data_directory + '/card-data.json'
-      if !File.exist?(data_directory)
-        Dir.mkdir(data_directory)
-        fetch_card_data(data_file)
-      elsif !File.exist?(data_file)
-        fetch_card_data(data_file)
-      else
-        puts 'Card data present; no download required'
-      end
+      data_file = DATA_DIRECTORY + '/card-data.json'
+      return if File.exists? data_file
+      Dir.mkdir(DATA_DIRECTORY) unless File.exists? DATA_DIRECTORY
+      fetch_card_data(data_file)
     end
 
     def fetch_card_data(destination)
-      tempfile = Downloader.get('http://mtgjson.com/json/AllSets.json')
+      tempfile = Downloader.get(DATA_SOURCE)
       file = File.new(destination, 'w')
       file.write(tempfile.read)
     end
